535b84d4c09f3f267e34d1c03ceb26865c443181
pablold <pablold@PAVANE_HP>
2022-11-05 19:15:07 +0100

    edito

--- tarea1casa_/src/esrudiantes/Vane.java
+++ tarea1casa_/src/esrudiantes/Vane.java
@@ -3,7 +3,8 @@ package esrudiantes;
 public class Vane {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola! Soy Vane (TXJ21094@educastur.es)");
+
 
 	}
 

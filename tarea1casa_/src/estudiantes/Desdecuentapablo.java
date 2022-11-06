247d5d9f3724dacc4020a4a868d167efb3a74116
pablold <pablold@PAVANE_HP.mshome.net>
2022-11-06 10:39:53 +0100

    des

--- tarea1casa_/src/estudiantes/Desdecuentapablo.java
+++ tarea1casa_/src/estudiantes/Desdecuentapablo.java
@@ -3,7 +3,7 @@ package estudiantes;
 public class Desdecuentapablo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-genevrated method stub
 
 	}
 
